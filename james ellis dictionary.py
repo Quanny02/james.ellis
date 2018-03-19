50c8f3b7b4b7285de8df92eb3c955600680cedca
James Delquan Ellis <james.ellisjr@stu.jefferson.kyschools.us>
2018-03-19 08:11:19 -0400

    commit message

---
+++ james ellis dictionary.py
@@ -0,0 +1,16 @@
+#James Ellis March 16, 2018
+
+
+My_Dict= {
+    'Humble': "Having or showing a moddest or low estimate of one's own importance.",
+    'Glory':"High renown or horror won by notable achievements.",
+    'Empathy':"The ability to understand and share the feelings of another",
+    'CLiche':"A phase or opinion that is over  used  and betrays a lack of original.",
+    'Allusion':"An expression designed to call something to mind without mentioning it explicitly; an indirect or passing reference.",
+    'Finesse':"intricate and refined delicacy",
+}
+
+
+for key in My_Dict:
+    print "key:" + str(key)
+    print "value:" + str(My_Dict[key])
